2b0e394f7fdc656ae882f45ee654b5207d48a967
Krissy <krissysaenz@gmail.com>
2020-05-04 13:24:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const movieData = require('./movie-data.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
+
+app.get('/movie', (req, res)=>{
+    const genre = req.query.genre;
+    const country = req.query.country;
+    const avg_vote = req.query.avg_vote;
+    let movies = movieData;
+
+    if(genre){
+        movies = movies.filter( movie => movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+    if(country){
+        movies = movies.filter( movie => movie.genre.toLowerCase().includes(country.toLowerCase()))
+    }
+    if(avg_vote){
+        movies=movies.filter( movie => movie.avg_vote>=Number(avg_vote))
+    }
+
+    res.json(movies)
+
+})
+
+
+app.listen('8000', ()=>{
+    console.log('App started on port 8000')
+})
